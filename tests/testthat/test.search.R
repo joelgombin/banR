979ef81685f8b2ece900e11b442fd37e7652b519
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2016-05-27 14:25:28 +0200

    update test files such as checking works

--- tests/testthat/test.search.R
+++ tests/testthat/test.search.R
@@ -2,16 +2,16 @@ library(banR)
 library(dplyr)
 context("search")
 
-test_that("ban_search_ works ", {
+test_that("ban_geocode_ works ", {
   data(paris2012)
   test_df <- paris2012 %>% slice(1:10) %>% mutate(adresse = paste(numero, voie, nom), code_insee = paste0("751", arrondissement))
-  expect_message(ban_search_(test_df, ~adresse, code_insee = "code_insee"), regexp = "Geocoding...")
-  expect_is(ban_search_(test_df, ~adresse, code_insee = "code_insee"), "tbl_df")
+  expect_message(ban_geocode_(test_df, ~adresse, code_insee = "code_insee"), regexp = "Geocoding...")
+  expect_is(ban_geocode_(test_df, ~adresse, code_insee = "code_insee"), "tbl_df")
 })
 
-test_that("ban_search works ", {
+test_that("ban_geocode works ", {
   data(paris2012)
   test_df <- paris2012 %>% slice(1:10) %>% mutate(adresse = paste(numero, voie, nom), code_insee = paste0("751", arrondissement))
-  expect_message(ban_search(test_df, adresse, code_insee = "code_insee"), regexp = "Geocoding...")
-  expect_is(ban_search(test_df, adresse, code_insee = "code_insee"), "tbl_df")
+  expect_message(ban_geocode(test_df, adresse, code_insee = "code_insee"), regexp = "Geocoding...")
+  expect_is(ban_geocode(test_df, adresse, code_insee = "code_insee"), "tbl_df")
 })
