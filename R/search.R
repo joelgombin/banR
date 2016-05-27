1060d32e894f5f1319a2f5b20acf52505246b6e6
Joel Gombin <joel.gombin@gmail.com>
2016-05-27 11:57:45 +0200

    name change for ban_search, fixes #2

--- R/search.R
+++ R/search.R
@@ -6,7 +6,7 @@
 #' @param code_postal nom d'une colonne contenant le code postal de la commune. Si \code{NULL} (par défaut), ce paramètre n'est pas utilisé. Si \code{code_insee} et \code{code_postal} sont tous les deux fournis, seul \code{code_insee} est fourni.
 #' @param URL base de l'URL de l'API à utiliser. Ne modifier qu'en cas de déploiement local ou sur un serveur distant de l'API BAN, distinct du point d'entrée offert par Etalab (\url{http://api-adresse.data.gouv.fr/}).
 #'
-#' @details la fonction \code{ban_search_} utilise une formule pour l'argument \code{adresses}, tandis que la fonction \code{ban_search} utilise un nom de colonne sans guillemets, à la dplyr.
+#' @details la fonction \code{ban_geocode_} utilise une formule pour l'argument \code{adresses}, tandis que la fonction \code{ban_geocode} utilise un nom de colonne sans guillemets, à la dplyr.
 #' @return un dataframe avec des colonnes supplémentaires avec la géolocalisation
 #' @import lazyeval
 #' @importFrom utils write.csv
@@ -18,9 +18,9 @@
 #'              slice(1:10) %>%
 #'              mutate(adresse = paste(numero, voie, nom),
 #'                     code_insee = paste0("751", arrondissement))
-#' ban_search(test_df, adresse, code_insee = "code_insee")
+#' ban_geocode(test_df, adresse, code_insee = "code_insee")
 #'
-ban_search_ <- function(data, adresses, code_insee = NULL, code_postal = NULL, URL = "http://api-adresse.data.gouv.fr/") {
+ban_geocode_ <- function(data, adresses, code_insee = NULL, code_postal = NULL, URL = "http://api-adresse.data.gouv.fr/") {
   # faire une sorte de test sur les adresses et éventuellement sur les IDs (??)
 
   # TODO : vérifier que le CSV est inférieur à 8 MO (limite de l'API)
@@ -64,8 +64,8 @@ ban_search_ <- function(data, adresses, code_insee = NULL, code_postal = NULL, U
 
 }
 
-#' @rdname ban_search_
+#' @rdname ban_geocode_
 #' @export
-ban_search <- function(data, adresses, code_insee = NULL, code_postal = NULL, URL = "http://api-adresse.data.gouv.fr/") {
-  ban_search_(data, lazyeval::f_capture(adresses), code_insee = code_insee, code_postal = code_postal, URL = URL)
+ban_geocode <- function(data, adresses, code_insee = NULL, code_postal = NULL, URL = "http://api-adresse.data.gouv.fr/") {
+  ban_geocode_(data, lazyeval::f_capture(adresses), code_insee = code_insee, code_postal = code_postal, URL = URL)
 }
