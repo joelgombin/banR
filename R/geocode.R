b002d2cff8f925cff5aee5f1dd430c520ec56b12
Paul-Antoine <pachevalier@gmail.com>
2017-08-01 17:25:30 +0200

    get_features, get_geometry and get_properties are not exported anymore. Only geocode and reverse geocode are exported

--- R/geocode.R
+++ R/geocode.R
@@ -4,20 +4,6 @@
 #' @param x a feature list
 #'
 #' @return a tibble
-#' @export
-#' 
-#' @examples
-#' library("magrittr")
-#' library("httr")
-#' library("tibble")
-#' library("purrr")
-#' test <- "http://api-adresse.data.gouv.fr/search/?q=8 bd du port" %>% 
-#' URLencode() %>% 
-#' GET() %>% 
-#' content()
-#' feature <- extract2(test, "features")[[1]]
-#' get_properties(feature)
-#' map_df(.x = test$features, .f = get_properties)
 #' 
 
 get_properties <- function(x) {
@@ -31,20 +17,6 @@ get_properties <- function(x) {
 #' @param x a feature
 #'
 #' @return a tibble
-#' @export
-#' 
-#' @examples
-#' library("magrittr")
-#' library("httr")
-#' library("tibble")
-#' library("purrr")
-#' test <- "http://api-adresse.data.gouv.fr/search/?q=8 bd du port" %>% 
-#' URLencode() %>% 
-#' GET() %>% 
-#' content()
-#' feature <- extract2(test, "features")[[1]]
-#' get_geometry(feature)
-#' map_df(.x = test$features, .f = get_geometry)
 #' 
 get_geometry <- function(x) {
   geom <- magrittr::extract2(x, "geometry")
@@ -64,18 +36,6 @@ get_geometry <- function(x) {
 #' @param x the content of a request
 #'
 #' @return a tibble
-#' @export
-#'
-#' @examples
-#' library("magrittr")
-#' library("httr")
-#' library("tibble")
-#' library("purrr")
-#' test <- "http://api-adresse.data.gouv.fr/search/?q=8 bd du port" %>% 
-#' URLencode() %>% 
-#' GET() %>% 
-#' content()
-#' get_features(test)
 #' 
 get_features <- function(x) {
   dplyr::bind_cols(
